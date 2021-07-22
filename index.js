cafccbcb1d129fdb1c07d0d4d1f1271eca4bdddf
Greg Testo <gregt@pursuit.org>
2021-07-22 14:32:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const users = require('./data/users.json');
+const todos = require('./data/todos.json');
+const posts = require('./data/posts.json');
+
+// Find the average amount saved by all of the users
+
+// Filter the posts by the given userId
+
+// Filter by all of the tasks that are incomplete
+
+// Advanced- Using a combination of the users, todos and posts arrays to find the answer.
+    // Return the user's name of who posted the most
+    // Return the user's name of who posted the least
+
+    // Return the user with the most number of completed todo items
+    // Return the user with the most number of completed todo items
+
+    // Filter the users with a given email domain i.e. jasper.info
